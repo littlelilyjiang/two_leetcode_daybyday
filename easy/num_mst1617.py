1cd37ce15b419923fbac42c57ad003b3a113f337
yan.li <yan.li@navercorp.com>
2020-10-19 17:17:24 +0800

    2020.10.19

---
+++ easy/num_mst1617.py
@@ -0,0 +1,11 @@
+'''
+给定一个整数数组，找出总和最大的连续数列，并返回总和。
+'''
+# SELECT
+#     (SELECT DISTINCT
+#             Salary
+#         FROM
+#             Employee
+#         ORDER BY Salary DESC
+#         LIMIT 1 OFFSET 1) AS SecondHighestSalary
+# ;
